dcf5767296c915a3a8dee5c2af78a3d58617fd19
jakeseg <117657251+jakeseg@users.noreply.github.com>
2024-12-02 15:01:39 -0500

    add nuxt-links and type definitions

--- composables/useExampleData.ts
+++ composables/useExampleData.ts
@@ -1,9 +1,27 @@
 import exampleData from './exampleData';
 
-export const useExampleData = () => {
-  const categories = exampleData.categories.map(
+export type Item = {
+  id: string; // Unique identifier for the service
+  title: string; // Title of the service being offered
+  description: string; // Description of the service
+  price: string; // Price for the service (e.g., "20,000 COP por hora")
+  contact: string; // Contact email of the user offering the service
+  location: string; // Physical or virtual location of the service
+  datePosted: string; // Date the service was posted (ISO format)
+};
+
+// Type definition for a category containing multiple items
+export type Category = {
+  slug: string; // URL slug for the category
+  name: string; // Name of the category (e.g., "Educación y Tutorías")
+  items: Item[]; // List of items under this category
+  count?: number; // Number of items in this category
+};
+
+export const useExampleData = (): Category[] => {
+  const categories: Category[] = exampleData.categories.map(
     (category) => {
-      const newItems = category.items.map(
+      const newItems: Item[] = category.items.map(
         (entry) => ({
           ...entry,
           path: `/detalles/${entry.id}/${entry.title}`,
@@ -18,5 +36,5 @@ export const useExampleData = () => {
       };
     }
   );
-  return categories;
+  return categories
 };
--- pages/categoria.vue
+++ pages/categoria.vue
@@ -12,13 +12,27 @@
       class="prose mr-4 p-8 bg-white rounded-md min-w-[20ch] max-w-[30ch] flex flex-col"
     >
       <h3>Categories</h3>
-
+      <NuxtLink
+          v-for="(category, index) in categories"
+          :key="category.slug"
+          class="flex flex-row space-x-1 no-underline prose-sm font-normal py-1 px-4 -mx-4"
+          :to="`/categoria/${category.slug}/entries`"
+          :class="{
+            'text-blue-500':
+              category.slug === $route.params.categoria,
+            'text-gray-600':
+              category.slug !== $route.params.categoria,
+          }"
+        >
+          <span class="text-gray-500"
+            >{{ index + 1 }}.</span
+          >
+          <span>{{ category.name }}</span>
+        </NuxtLink>
     </div>
 
     <div class="prose p-12 bg-white rounded-md w-[65ch]">
-      <pre>
-        {{ categories }}
-      </pre>
+
     </div>
   </div>
 </template>
